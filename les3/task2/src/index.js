1632ee9c7c8523f8219a4bfcaf2fcfe99ac78a3c
Vitalii1706 <kitral.v.o@gmail.com>
2022-02-13 19:20:07 +0200

    1

---
+++ les3/task2/src/index.js
@@ -0,0 +1,9 @@
+import './index.scss';
+import { store, increment, decrement } from './counter';
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(increment());
+store.dispatch(increment());
