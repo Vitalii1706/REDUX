a578b5e58a91c420caf90a323c67f4b971a57eb0
Vitalii1706 <kitral.v.o@gmail.com>
2022-02-15 14:53:34 +0200

    1

---
+++ les5/task1/src/users/UsersList.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as userActions from './users.actions';
+import User from './User.jsx';
+import Pagination from './Pagination.jsx';
+import { usersListSelector, currentPageSelector } from './users.selectors';
+
+class UsersList extends Component {
+  render() {
+    const { users, currentPage, goNext, goPrev } = this.props;
+    const itemsPerPage = 3;
+    const usersToRender = users.slice(
+      currentPage * itemsPerPage,
+      itemsPerPage + currentPage * itemsPerPage,
+    );
+
+    return (
+      <div>
+        <Pagination
+          goPrev={goPrev}
+          goNext={goNext}
+          currentPage={currentPage}
+          totalItems={users.length}
+          itemsPerPage={itemsPerPage}
+        />
+        <ul className="users">
+          {usersToRender.map(user => (
+            <User key={user.id} {...user}></User>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+const mapState = state => {
+  return {
+    users: usersListSelector(state),
+    currentPage: currentPageSelector(state),
+  };
+};
+
+const mapDispatch = {
+  goNext: userActions.goNext,
+  goPrev: userActions.goPrev,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+const Users = connector(UsersList);
+
+export default Users;
