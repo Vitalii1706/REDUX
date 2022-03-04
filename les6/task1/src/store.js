95d0b33c5d21dfb9f6451b83c5ef8f1ce5252da3
Vitalii1706 <kitral.v.o@gmail.com>
2022-03-04 20:34:46 +0200

    3

--- les6/task1/src/store.js
+++ les6/task1/src/store.js
@@ -1,17 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import counterReducer from './counter.reducer';
+import { counterReducer } from './counter.reducer';
 
-const logger = store => next => action => {
+const logger = state => next => action => {
   console.group(action.type);
   console.info('dispatching', action);
-  const result = next(action);
-  console.log('new state', store.getState());
+  let result = next(action);
+  console.log('next state', store.getState());
   console.groupEnd();
   return result;
 };
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const store = createStore(counterReducer, composeEnhancers(applyMiddleware(logger)));
-
 export default store;
