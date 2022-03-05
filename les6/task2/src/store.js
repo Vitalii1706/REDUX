8a01a4eb98ef160cf74c691117cb2b1d8c6c887d
Vitalii1706 <kitral.v.o@gmail.com>
2022-03-05 19:28:41 +0200

    3

--- les6/task2/src/store.js
+++ les6/task2/src/store.js
@@ -2,18 +2,19 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './users/users.reducer';
 
-const reducer = combineReducers({
-  users: usersReducer,
-});
-
 const logger = store => next => action => {
   console.group(action.type);
   console.info('dispatching', action);
   let result = next(action);
-  console.log('new state', store.getState());
+  console.log('next state', store.getState());
   console.groupEnd();
   return result;
 };
+
+const reducer = combineReducers({
+  users: usersReducer,
+});
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
