fbb2655e74455e572e6f4db0d694a64c2cab1e1f
Vitalii1706 <kitral.v.o@gmail.com>
2022-03-05 17:38:16 +0200

    1

---
+++ les6/task2/src/users/UserInfo.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Spinner from './Spinner';
+import { isFetchingSelector, userDataSelector } from './users.selectors';
+import PropTypes from 'prop-types';
+/*import * as userActions from './users.actions';
+import Pagination from './Pagination.jsx';
+*/
+
+const UserInfo = ({ userData, isFetching }) => {
+  if (isFetching) {
+    return <Spinner />;
+  }
+  if (!userData) {
+    return null;
+  }
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={userData.avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{userData.name}</span>
+        <span className="user__location">{`from ${userData.location}`}</span>
+      </div>
+    </div>
+  );
+};
+
+UserInfo.PropTypes = {
+  userData: PropTypes.shape(),
+  isFetching: PropTypes.bool.isRequired,
+};
+
+UserInfo.defaultValue = {
+  userData: null,
+};
+
+const mapState = state => {
+  return {
+    isFetching: isFetchingSelector(state),
+    userData: userDataSelector(state),
+  };
+};
+
+export default connect(mapState)(UserInfo);
